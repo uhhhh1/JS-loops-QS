63d59e9258bfc2e5009b0698a58ac654034e1bc1
chloe <chloep2838@tuckahoeschools.org>
2024-12-04 13:32:17 +0000

    initial commit

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,24 @@
+let outputDiv = document.getElementById("output");
+let passwordInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("submit-button");
 
+const correctPassword = "skibuddy";
+outputDiv.innerHTML += "<h2>Using a password prompt</h2>";
+
+submitButton.addEventListener("click", () =>{
+    let password;
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Access granted! <br>"
+            passwordInput.disabled = true;
+            submitButton.disabled = true;
+            break;
+        }else{
+            outputDiv.innerHTML += ''
+        }
+    }
+});
